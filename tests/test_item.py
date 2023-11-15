f29597ffc9e52dd156c5d3758cd805d434c54623
ElnaraGasanova <elnaraskypro@gmail.com>
2023-11-16 00:30:49 +0300

    Реализован класс Item. Написаны тесты.

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_item():
+    item1 = Item("Samsung", 15000, 10)
+    assert item1.name == "Samsung"
+    assert item1.price == 15000
+    assert item1.quantity == 10
+    assert item1.calculate_total_price() == 150000
+    Item.pay_rate = 0.5
+    item1.apply_discount()
+    assert item1.price == 7500
