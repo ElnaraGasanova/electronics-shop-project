d5a098007025823463a3d08015be18607a10cac7
ElnaraGasanova <elnaraskypro@gmail.com>
2023-11-16 00:56:26 +0300

    Дописаны тесты.

--- tests/test_item.py
+++ tests/test_item.py
@@ -3,11 +3,21 @@ from src.item import Item
 
 
 def test_item():
-    item1 = Item("Samsung", 15000, 10)
-    assert item1.name == "Samsung"
-    assert item1.price == 15000
-    assert item1.quantity == 10
-    assert item1.calculate_total_price() == 150000
-    Item.pay_rate = 0.5
+    item1 = Item("Смартфон", 10000, 20)
+    assert item1.name == "Смартфон"
+    assert item1.price == 10000
+    assert item1.quantity == 20
+    assert item1.calculate_total_price() == 200000
+    Item.pay_rate = 0.8
     item1.apply_discount()
-    assert item1.price == 7500
+    assert item1.price == 8000
+
+    item2 = Item("Ноутбук", 20000, 5)
+    assert item2.name == "Ноутбук"
+    assert item2.price == 20000
+    assert item2.quantity == 5
+    assert item2.calculate_total_price() == 100000
+    Item.pay_rate = 0.8
+    item2.apply_discount()
+    assert item2.price == 16000
+    assert Item.all == [item1, item2]
